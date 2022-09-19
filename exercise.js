4bb7316589647b94d2b679d9db36c653a3d1167f
Aiman Al Eryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-19 16:27:43 +0300

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -11,7 +11,8 @@
 
 // console.log(newUser);
 // console.log(user);
-// there is the best way by using the JSON object, we change the "data" object to a json data and then parse the json data to create the new object ... the good thing about this method that it creates a full new object "deep copy" and not a "shallow copy"
+// the best way is by using the JSON object, we change the "data" object to a json data and then parse the json data to create the new object ... 
+// the good thing about this method is that it creates a full new object "deep copy" and not a "shallow copy"
 
 let user = {
   name: "Cosimo",
